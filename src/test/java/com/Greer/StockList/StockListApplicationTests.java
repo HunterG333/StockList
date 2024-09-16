9d797285481c4c023480851df7521fa70f919935
HunterG333 <huntergreer03@gmail.com>
2024-09-15 19:17:02 -0700

    Initial Commit

---
+++ src/test/java/com/Greer/StockList/StockListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Greer.StockList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
